426edba829191827df5ab50699b66c4828298ecc
Maksym Potapchuk <91498166+MacMicroSoft@users.noreply.github.com>
2024-05-23 18:14:58 +0300

    Added uncommit changes

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Restaurant, Employee, Menu, Vote
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_employee', 'is_restaurant')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Restaurant)
+admin.site.register(Employee)
+admin.site.register(Menu)
+admin.site.register(Vote)
